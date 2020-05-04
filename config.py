bd9a97b6d4ca15af886956c581bc67142ac65b3d
Essy Mwangi <sonnieessy@gmail.com>
2020-05-04 17:37:47 +0300

    heroku

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://essie:Monica/19@localhost/pblog'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
